8bf62aa33ae6a5141ece7498b45bff4f04bd77ad
Grant Montgomery <lettucebowler@gmail.com>
2024-04-12 06:41:02 +0000

    backup only if volumes exist for stack in /opt/docker/volumes/<stack>

--- backup.sh
+++ backup.sh
@@ -4,20 +4,23 @@ for f in /opt/docker/stacks/*;
   do
     ff=${f##*/}
     echo Backing up ${ff}
-    echo ${f}/docker-compose.yml
     docker-compose --file ${f}/docker-compose.yml down
     if test -f /opt/docker/backup/backups/$ff.tar.lz4; then
       rm /opt/docker/backup/backups/$ff.tar.lz4
     fi
-    tar -cvf - -C /opt/docker/volumes/$ff . | lz4 - /opt/docker/backup/backups/$(hostname).$ff.tar.lz4
+    if test -d /opt/docker/volumes/$ff; then
+      tar -cvf - -C /opt/docker/volumes/$ff . | lz4 - /opt/docker/backup/backups/$(hostname).$ff.tar.lz4
+    fi
     docker-compose -f $f/docker-compose.yml pull
     docker-compose -f $f/docker-compose.yml up -d --remove-orphans
 done
 for f in /opt/docker/stacks/*;
   do
-    ff=${f##*/}
-    echo Moving /opt/docker/backup/backups/$(hostname).$ff.tar.lz4 to Backups share
-    cp /opt/docker/backup/backups/$(hostname).$ff.tar.lz4 /mnt/truenas/backups/
-    rm /opt/docker/backup/backups/$(hostname).$ff.tar.lz4
+    if test -d /opt/docker/volumes/$ff; then
+      ff=${f##*/}
+      echo Moving /opt/docker/backup/backups/$(hostname).$ff.tar.lz4 to Backups share
+      cp /opt/docker/backup/backups/$(hostname).$ff.tar.lz4 /mnt/truenas/backups/
+      rm /opt/docker/backup/backups/$(hostname).$ff.tar.lz4
+    fi
 done
 docker system prune -af
