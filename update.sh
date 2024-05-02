cb82e6a275ffd9d9d6942df5cbb39adec5c8ff4b
Grant Montgomery <lettucebowler@gmail.com>
2024-05-01 23:56:11 -0500

    bleh

---
+++ update.sh
@@ -0,0 +1,17 @@
+cd /opt/docker/volumes
+ff=""
+for f in /opt/docker/stacks/*;
+  do
+    ff=${f##*/}
+    echo Backing up ${ff}
+    docker compose --file ${f}/docker-compose.yml down
+    if test -f /opt/docker/backup/backups/$ff.tar.lz4; then
+      rm /opt/docker/backup/backups/$ff.tar.lz4
+    fi
+    if test -d /opt/docker/volumes/$ff; then
+      tar -cvf - -C /opt/docker/volumes/$ff . | lz4 - /opt/docker/backup/backups/$(hostname).$ff.tar.lz4
+    fi
+    docker compose -f $f/docker-compose.yml pull
+    docker compose -f $f/docker-compose.yml up -d --remove-orphans
+done
+docker system prune -af
