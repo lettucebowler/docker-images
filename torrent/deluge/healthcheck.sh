868af6d6ce4e2580a211b2b73e7455be34993179
Grant Montgomery <lettucebowler@gmail.com>
2024-05-24 20:45:48 +0000

    bleh

--- torrent/deluge/healthcheck.sh
+++ torrent/deluge/healthcheck.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-pkill deluge-console
-deluge-console --username user --password pass status | grep "^DHT Nodes: 0$"
-if [ $? -eq 0 ]; then
-  pkill deluged
-  exit 1
-fi
+# pkill deluge-console
+# deluge-console --username user --password pass status | grep "^DHT Nodes: 0$"
+# if [ $? -eq 0 ]; then
+#   pkill deluged
+#   exit 1
+# fi
+echo ${DELUGE_USER}
